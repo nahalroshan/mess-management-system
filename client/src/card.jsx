a974ad6aca46f0d6f63a0154b0018e939519066a
Nandana B Pillai <90125324+Nandanabpillai@users.noreply.github.com>
2023-11-13 17:19:38 +0530

    dashboard

---
+++ client/src/card.jsx
@@ -0,0 +1,19 @@
+export default function Card(props) {
+    return (
+        <div className="w-44 h-40 shadow-lg shadow-dark-gray rounded-xl bg-white flex flex-col justify-between">
+            <div className="flex mt-4 items-center">
+                <div className="ml-4 w-8 h-8 bg-pink-icon-bg rounded-full flex items-center justify-center">
+                    <box-icon name={props.icon} color='#CD6496'></box-icon>
+                </div>
+                <div className="ml-2 flex flex-col">
+                    <div className="font-medium text-dark-dark-gray">{props.title}</div>
+                    <div className="text-dark-gray text-xs">This Week</div>
+                </div>
+            </div>
+            <div className="text-right text-3xl mb-4 mr-4 text-dark-dark-gray">{props.count}</div>
+        </div>
+
+      
+    );
+  }
+  
