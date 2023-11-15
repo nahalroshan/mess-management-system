059f66bf18bc54a4904b4de4d41f5a80a68cdfb8
Nandana B Pillai <90125324+Nandanabpillai@users.noreply.github.com>
2023-11-15 15:56:32 +0530

    Update signup page

--- client/src/register.jsx
+++ client/src/register.jsx
@@ -24,25 +24,27 @@ export default function Register(props) {
     }
 
     return (
-        <div className="flex flex-col w-96 border-2 border-black space-y-8">
-            <div className="text-3xl font-medium ml-6 mt-8">Signup</div>
+        <div className="flex bg-blue-400 h-screen w-full">
+            <div className="flex flex-col shadow-xl w-96 bg-white h-3/4 border-2 border-white mx-auto my-8 space-y-8 rounded-md mt-24">
+            <div className="text-2xl ml-8 mt-8 ">Signup</div>
             <form onSubmit={handleSubmit}>
-                <div className="flex flex-col  ml-6">
-                    <label className = 'text-lg' htmlFor = 'name'>User Name</label>
-                    <input className="border-2 border-black w-3/4" value = {name} onChange={(e) => setName(e.target.value)} id="name" name="name"></input>
+                <div className="flex flex-col  ml-8 ">
+                    <label className = 'text-lg mb-2 ml-1' htmlFor = 'name'>User Name</label>
+                    <input className="rounded-md  border-2 w-72 h-9" value = {name} onChange={(e) => setName(e.target.value)} id="name" name="name"></input>
                 </div>
-                <div className="flex flex-col  ml-6">
-                    <label className = 'text-lg' htmlFor = 'email'>Email</label>
-                    <input className="border-2 border-black w-3/4" value = {email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email"></input>
+                <div className="flex flex-col  ml-8">
+                    <label className = 'text-lg mb-2 mt-2 ml-1' htmlFor = 'email'>Email</label>
+                    <input className="rounded-md  border-2 w-72 h-9" value = {email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email"></input>
                 </div>
-                <div className="flex flex-col  ml-6">
-                    <label className = 'text-lg' htmlFor = 'password'>Password</label>
-                    <input className="border-2 border-black w-3/4" value = {pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password"></input> 
+                <div className="flex flex-col  ml-8">
+                    <label className = 'text-lg mb-2 mt-2 ml-1' htmlFor = 'password'>Password</label>
+                    <input className="rounded-md  border-2 w-72 h-9" value = {pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password"></input> 
                 </div>
-                <button className = 'border-2 border-black mt-6 ml-7 w-64' type="submit"> Register </button>
+                <button className="border-2  mt-6 ml-8 w-72 p-2 rounded-md bg-pink-500 text-white hover:bg-pink-700" type="submit"> Register </button>
             </form>
             <div>
-                <button className="text-center mb-8 text-md"> Already have an account? <span className="font-medium underline decoration-solid"><Link to = "/">Login here</Link></span></button>
+                <button className="text-center mb-8 text-md ml-12"> Already have an account? <span className="font-medium underline decoration-solid"><Link to = "/">Login here</Link></span></button>
+            </div>
             </div>
         </div>
                 
