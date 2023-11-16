e8e18c80c07e03797db2a51582150c1ac92edc0a
Nandana B Pillai <90125324+Nandanabpillai@users.noreply.github.com>
2023-11-16 18:50:31 +0530

    Update home.jsx

--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -3,8 +3,44 @@ import Card from "./card"
 import Search from "./search"
 import Table from './table'
 import Stats from "./stats"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase"
+import { useState, useEffect } from "react"
 
 export default function Home() {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    // Listen for changes to the authentication state
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        getUsernameFromFirestore(user.email);
+      }
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  const getUsernameFromFirestore = async (email) => {
+    try {
+      const usersCollection = collection(db, "usernames");
+      const q = query(usersCollection, where("email", "==", email));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        // Assuming there's only one matching document
+        const docData = querySnapshot.docs[0].data();
+        setUsername(docData.name);
+      } else {
+        console.log("No matching documents.");
+      }
+    } catch (error) {
+      console.error("Error getting username:", error);
+    }
+  };
+
   return (
     <div className="bg-light-gray h-screen w-screen flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-28">
       <div className="w-full sm:w-40">
@@ -15,11 +51,11 @@ export default function Home() {
           <p className="text-3xl font-medium ml-10">Dashboard</p>
           <div className="flex flex-row space-x-4 items-center">
             <div className="rounded-full w-10 h-10 flex items-center justify-center bg-white"><box-icon name='envelope' color='#CD6496'></box-icon></div>
-            <div className="space-x-4 rounded-l-2xl bg-white w-56 h-16 flex flex-row items-center shadow-lg shadow-dark-gray">
-              <div className="ml-4"><box-icon name='user-circle' size='lg'></box-icon></div>
+            <div className="space-x-4 rounded-l-2xl bg-white w-72 h-16 flex flex-row items-center shadow-lg shadow-dark-gray">
+              <div className="ml-4"><img src={user.photoURL} alt="User" /></div>
               <div className="flex flex-col">
-                <div> John Doe</div>
-                <div className="text-xs text-dark-gray"> johndoe@gmail.com </div>
+                <div> {username} </div>
+                <div className="text-xs text-dark-gray"> {user.email} </div>
               </div>
             </div>
           </div>
